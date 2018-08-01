b54aead30a58e2368a59c5fc7f3aa84b21146c05
Luca1661432 <luca.digian@hotmail.com>
2018-08-01 17:52:06 +0200

    Aggiungta MVC Controllers per Autori e Libri

---
+++ src/main/java/com/springcourse/Spring5Web/controller/AuthorController.java
@@ -0,0 +1,21 @@
+package com.springcourse.Spring5Web.controller;
+
+import com.springcourse.Spring5Web.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository){
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
